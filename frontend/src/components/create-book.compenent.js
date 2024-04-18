ac543697a2c11fbc0b705811f8d3707cba375fba
SubashiniPiumali <hewaihalages@student.douglascollege.ca>
2024-04-17 20:54:53 -0700

    front api change

--- frontend/src/components/create-book.compenent.js
+++ frontend/src/components/create-book.compenent.js
@@ -11,7 +11,7 @@ export default function CreateBook() {
     e.preventDefault();
 
     let book = {title: title , author: author, description: description};
-    const url = "http://localhost:5000/api";
+    const url = "https://full-stack-p.vercel.app/api";
     axios
       .post(url, book)
       .then((res) => {
